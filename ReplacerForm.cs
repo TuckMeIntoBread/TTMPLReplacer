25078565bbf92ed8a3db32a378e07a8ee783e897
TuckMeIntoBread <TuckMeIntoBread@users.noreply.github.com>
2022-02-15 13:19:14 -1000

    init

---
+++ ReplacerForm.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace TTMPLReplacer
+{
+    public partial class ReplacerForm : Form
+    {
+        public ReplacerForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
